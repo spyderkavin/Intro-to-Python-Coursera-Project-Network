642e93aa4e135dd3cde55fad9696705f9319d789
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2025-06-03 17:36:05 -0700

    3rd video completed

--- guessingGame.py
+++ guessingGame.py
@@ -1,5 +1,12 @@
-
+import random
 #get the user's guess
 guess = int(input("Enter your guess between 1 and 100: "))
 
+randnum = randint(1, 101)
 
+if guess < randnum:
+    print("higher")
+elif guess > randnum:
+    print("lower")
+else:
+    print("You guessed it!")
