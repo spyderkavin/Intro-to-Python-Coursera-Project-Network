76491f519e18ef828032185e66f8d0c893ceca18
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2025-06-03 18:06:10 -0700

    Challenge Task: While Loops and Functions

--- scores/scores.py
+++ scores/scores.py
@@ -1,7 +1,11 @@
 def scores(score):
-    if score >= 50:
-        print("Pass")
-    elif score < 50:
-        print("Fail")
-    
+    while score >=0:    
+        if score >= 50:
+            print("Pass")
+        elif score < 50 and score >= 0:
+            print("Fail")
+        elif score < 0:
+            print("Invalid score")
+            break
+        
 scores(int(input("Enter your score: ")))  # Example usage
