04a1036c2bcd2eb24da858948de5a9088b3ba9b3
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2025-06-03 17:53:18 -0700

    Task 4 Completed

--- guessingGame.py
+++ guessingGame.py
@@ -1,12 +1,19 @@
 import random
 #get the user's guess
-guess = int(input("Enter your guess between 1 and 100: "))
 
-randnum = randint(1, 101)
+randnum = random.randint(1, 101)
 
-if guess < randnum:
-    print("higher")
-elif guess > randnum:
-    print("lower")
-else:
-    print("You guessed it!")
+count = 0
+guess = 0
+while guess != randnum:
+    guess = int(input("Enter your guess between 1 and 100: "))
+    if guess < randnum:
+        print("higher")
+    elif guess > randnum:
+        print("lower")
+    else:
+        print("You guessed it!")
+        break
+    count += 1
+print(f"You guessed it in {count} tries!")
+       
