16a3832fe927107c7b0e37bf532f59e3ef2dedc1
Kavinkumar Sainathan <kavincoolsout@gmail.com>
2025-06-03 18:09:38 -0700

    Challenge Task 2 Completed

--- scores/scores.py
+++ scores/scores.py
@@ -1,11 +1,16 @@
-def scores(score):
-    while score >=0:    
-        if score >= 50:
-            print("Pass")
-        elif score < 50 and score >= 0:
-            print("Fail")
-        elif score < 0:
-            print("Invalid score")
-            break
+def scores(score): 
+    if score >= 50:
+        print("Pass")
         
-scores(int(input("Enter your score: ")))  # Example usage
+    elif score < 50 and score >= 0:
+        print("Fail")
+    elif score < 0:
+        print("Invalid score")
+
+score = int(input("Enter your score: "))
+
+while score >=0:
+    scores(score)  # Example usage
+    score = int(input("Enter your score: "))
+
+    
